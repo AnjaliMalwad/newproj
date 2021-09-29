0b4be0c5ef0900d04ec964b13b5b4f5579119ba8
AnjaliMalwad <91150266+AnjaliMalwad@users.noreply.github.com>
2021-09-29 14:34:55 +0530

    Add files via upload

---
+++ employee.java
@@ -0,0 +1,17 @@
+package Day1_assignment1;
+import java.util.Scanner;
+public class employee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		    Scanner s=new Scanner(System.in);
+		    System.out.println("enter your name");
+		    String name=s.next();
+		    System.out.println("enter your Age");
+		    int age =s.nextInt();
+		    System.out.println("Name="+ name);
+		    System.out.println("Age="+ age);
+		 
+	}
+
+}
